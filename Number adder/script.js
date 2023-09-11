c2bdafa25182f2c01eaa41b723760f3dc6fce93c
AsmanHud <asman.hudaykulyyev@stud.vilniustech.lt>
2023-09-11 02:51:56 +0200

    Major fixes

--- Number adder/script.js
+++ Number adder/script.js
@@ -4,29 +4,42 @@ const mainForm = document.querySelector('#main-form');
 const resultSum = document.querySelector('#result');
 const inpElem = mainForm['inp'];  // Get the input element
 
-const charsRegex = /[\s!\"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/g;
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
 
-function calcSum(text) {
-    const cleanText = text.replace(charsRegex, ' ');
-    let numbers = cleanText.split(/\s+/).map(x => parseInt(x)).filter(x => !isNaN(x));
-    let sum = numbers.reduce((acc, curr) => acc + curr, 0);
+function calcSum() {
+    let sum = 0;
+    const words = inpElem.value.replaceAll(',', '.').split(/\s+/);
+    for (let word of words) {
+        if (isNumeric(word)) {
+            sum += Number(word);
+        }
+    }
+    if (sum % 1 !== 0) {
+        const decimalPlaces = words
+            .filter(elem => elem.includes('.'))
+            .map(elem => elem.slice(elem.indexOf('.') + 1).length);
+        const maxDecimalPlaces = Math.max(...decimalPlaces);
+        sum = sum.toFixed(maxDecimalPlaces); // returns a string!
+    }
     resultSum.innerText = sum;
 }
 
-mainForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    calcSum(inpElem.value);
-})
-
-mainForm.addEventListener('input', () => {
-    calcSum(inpElem.value);
-})
-
-resetBtn.addEventListener('click', () => {
-    resultSum.innerText = 0;
+function reset() {
+    resultSum.innerText = '0';
     inpElem.value = '';
-})
+}
+
+mainForm.addEventListener('submit', (e) => { e.preventDefault(); })
+
+mainForm.addEventListener('input', calcSum)
+
+resetBtn.addEventListener('click', reset)
 
 calcBtn.addEventListener('click', () => {
+    reset()
     inpElem.value = 'Пизда!!!';  // Be cautious with this value
 })
