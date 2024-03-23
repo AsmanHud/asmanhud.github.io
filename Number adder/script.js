812b81359caeccc56cbab43da1b820f9c07ff46e
AsmanPooon <100230676+AsmanHud@users.noreply.github.com>
2024-03-24 01:21:36 +0200

    Major update - Remove some websites - Refine the global styles file, add grid system for buttons - and more

--- Number adder/script.js
+++ Number adder/script.js
@@ -1,18 +1,20 @@
-const calcBtn = document.querySelector('#calc');
-const resetBtn = document.querySelector('#reset');
-const mainForm = document.querySelector('#main-form');
-const resultSum = document.querySelector('#result');
-const inpElem = mainForm['inp'];  // Get the input element
+const calcBtn = document.querySelector("#calc");
+const resetBtn = document.querySelector("#reset");
+const mainForm = document.querySelector("#main-form");
+const resultSum = document.querySelector("#result");
+const inpElem = mainForm["inp"]; // Get the input element
 
 function isNumeric(str) {
-    if (typeof str != "string") return false // we only process strings!  
-    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+    if (typeof str != "string") return false; // we only process strings!
+    return (
+        !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str))
+    ); // ...and ensure strings of whitespace fail
 }
 
 function calcSum() {
     let sum = 0;
-    const words = inpElem.value.replaceAll(',', '.').split(/\s+/);
+    const words = inpElem.value.replaceAll(",", ".").split(/\s+/);
     for (let word of words) {
         if (isNumeric(word)) {
             sum += Number(word);
@@ -20,8 +22,8 @@ function calcSum() {
     }
     if (sum % 1 !== 0) {
         const decimalPlaces = words
-            .filter(elem => elem.includes('.'))
-            .map(elem => elem.slice(elem.indexOf('.') + 1).length);
+            .filter((elem) => elem.includes("."))
+            .map((elem) => elem.slice(elem.indexOf(".") + 1).length);
         const maxDecimalPlaces = Math.max(...decimalPlaces);
         sum = sum.toFixed(maxDecimalPlaces); // returns a string!
     }
@@ -29,17 +31,19 @@ function calcSum() {
 }
 
 function reset() {
-    resultSum.innerText = '0';
-    inpElem.value = '';
+    resultSum.innerText = "0";
+    inpElem.value = "";
 }
 
-mainForm.addEventListener('submit', (e) => { e.preventDefault(); })
+mainForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
 
-mainForm.addEventListener('input', calcSum)
+mainForm.addEventListener("input", calcSum);
 
-resetBtn.addEventListener('click', reset)
+resetBtn.addEventListener("click", reset);
 
-calcBtn.addEventListener('click', () => {
-    reset()
-    inpElem.value = 'Пизда!!!';  // Be cautious with this value
-})
+calcBtn.addEventListener("click", () => {
+    reset();
+    alert("This button's functionality is in development currently...");
+});
